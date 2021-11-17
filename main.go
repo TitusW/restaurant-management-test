e4ba66661194322969babbebe2e32eb6b0515219
Titus <tituswus@gmail.com>
2021-11-17 15:27:58 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/middleware"
+	"golang-restaurant-management/routes"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/gin-gonic/gin"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
